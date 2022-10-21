6e3a3fa4f6e82b2fd27335cea48864df20263247
Pawel-9215 <hordyniak@gmail.com>
2022-10-21 18:15:45 +0200

    hader implementation

--- server_01.py
+++ server_01.py
@@ -1,5 +1,6 @@
 import socket
 
+HEADERSIZE = 10
 my_ip = ""
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,3 +20,5 @@ while True:
     print(f"connection from {address} established")
     client_sock.send(bytes("Welcome to the server", 'utf-8'))
     client_sock.send(bytes("New message", 'utf-8'))
+    new_msg = client_sock.recv(1024)
+    print(new_msg.decode('utf-8'))
