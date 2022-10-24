e38a4ef7e2356acb4143742487635e5309d28758
Pawel-9215 <hordyniak@gmail.com>
2022-10-24 16:36:32 +0200

    ready for field testing

--- servo_control.py
+++ servo_control.py
@@ -4,6 +4,22 @@ from numpy import interp
 
 # setup
 servo_1 = HardwarePWM(pwm_channel=0, hz=50)
+servo_1.start(100)
+
+def set_steering(steering_val):
+    duty = interp(steering_val, [-255, 255], [2.5, 10.5])
+    servo_1.change_duty_cycle(duty)
+
+def set_acceleration(acc_val):
+    pass
+
+def set_reverse(rev_val):
+    pass
+
+def set_input_params(input_params):
+    set_steering(input_params['steering'])
+    set_acceleration(input_params['acceleration'])
+    set_reverse(input_params['reverse'])
 
 
 if __name__ == '__main__':
