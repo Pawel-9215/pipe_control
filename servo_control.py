b6d1e5978c9bd2f62cae5e8f1a922dde680acd3c
Your Name <you@example.com>
2022-10-25 18:39:36 +0200

    code for engines

--- servo_control.py
+++ servo_control.py
@@ -6,20 +6,29 @@ from numpy import interp
 servo_1 = HardwarePWM(pwm_channel=0, hz=50)
 servo_1.start(6.5)
 
+engines = HardwarePWM(pwm_channel=1, hz=60)
+engines.start(0)
+
 def set_steering(steering_val):
     duty = interp(steering_val, [-255, 255], [2.5, 10.5])
     servo_1.change_duty_cycle(duty)
 
-def set_acceleration(acc_val):
-    pass
-
-def set_reverse(rev_val):
-    pass
+def set_movement(acc_val, rev_val):
+    if acc_val > 0 and rev_val 0 = 0:
+        speed =  acc_val
+        pass # set forward pin
+    if acc_val == 0 and rev_val >= 0:
+        speed = rev_val
+        pass # set backward pin
+    else:
+        speed = 0
+    
+    engines.change_duty_cycle(interp(steering, [0, 255], [0, 100]))
 
 def set_input_params(input_params):
     set_steering(input_params['steering'])
-    set_acceleration(input_params['acceleration'])
-    set_reverse(input_params['reverse'])
+    set_movement(input_params['acceleration'], input_params['reverse'])
+
 
 
 if __name__ == '__main__':
